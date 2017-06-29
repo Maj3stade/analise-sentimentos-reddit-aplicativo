c46c5d9b33e9d227f7fdb6ee96a3b251d5141caf
Maj3stade <guilhermeandreata@hotmail.com>
2017-06-28 22:22:36 -0300

    Ajustes

--- src/crawler/Crawler.java
+++ src/crawler/Crawler.java
@@ -4,7 +4,6 @@ import java.util.Iterator;
 import java.util.List;
 
 import crawler.entity.RedditPost;
-import crawler.entity.RedditThread;
 import crawler.properties.RedditProperties;
 import manager.RedditManager;
 
@@ -14,23 +13,17 @@ public class Crawler {
 	public static void main(String[] args) {
 		
 		try {
-			List<RedditThread> threadList = Requestor.getThreads(RedditProperties.HTTPS_REDDIT + "r/games/top");
-			RedditManager.saveThreads(threadList);
-			for (Iterator<RedditThread> iterator = threadList.iterator(); iterator.hasNext();) {
-				RedditThread redditThread = (RedditThread) iterator.next();
-				List<RedditPost> postList = Requestor.getPosts(RedditProperties.HTTPS_REDDIT + redditThread.getId());
-				System.out.println("Comentários " + postList.size());
+			for (Iterator<String> iterator = RedditProperties.SELECTED_THREADS.iterator(); iterator.hasNext();) {
+				List<RedditPost> postList = Requestor.getPosts(RedditProperties.HTTPS_REDDIT + (String) iterator.next());
+				System.out.println("Comentarios " + postList.size());
 				RedditManager.savePosts(postList);
 			}
 			
 			RedditManager.shutdown();
 			
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		// TODO Auto-generated method stub
-
 	}
 
 }
--- src/crawler/properties/RedditProperties.java
+++ src/crawler/properties/RedditProperties.java
@@ -1,7 +1,32 @@
 package crawler.properties;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class RedditProperties {
 
 	public static final String HTTPS_REDDIT = "https://www.reddit.com/";
+	
+	public static final List<String> SELECTED_THREADS = new ArrayList<>();
+	
+	//Tópicos Selecionados Através do TCC
+	public RedditProperties(){
+		SELECTED_THREADS.add("36ny58");
+		SELECTED_THREADS.add("4bqcc3");
+		SELECTED_THREADS.add("5oyz3b");
+		SELECTED_THREADS.add("21f3as");
+		SELECTED_THREADS.add("2t65ql");
+		SELECTED_THREADS.add("4d75i7");
+		SELECTED_THREADS.add("5exz2e");
+		SELECTED_THREADS.add("67ivae");
+		SELECTED_THREADS.add("6cqdye");
+		SELECTED_THREADS.add("5uzetf");
+		SELECTED_THREADS.add("5qqa51");
+		SELECTED_THREADS.add("5whpqs");
+		SELECTED_THREADS.add("1wl9ic");
+		SELECTED_THREADS.add("2s7obx");
+		SELECTED_THREADS.add("5n58sm");
+	}
+	
 
 }
