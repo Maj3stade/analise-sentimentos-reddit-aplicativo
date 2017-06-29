d85b029d9139fdcdab7bd0b259d3fc18b058207c
Maj3stade <guilhermeandreata@hotmail.com>
2017-06-29 19:56:29 -0300

    Correção

--- src/crawler/properties/RedditProperties.java
+++ src/crawler/properties/RedditProperties.java
@@ -11,21 +11,21 @@ public class RedditProperties {
 	
 	//Tópicos Selecionados Através do TCC
 	public RedditProperties(){
-		SELECTED_THREADS.add("36ny58");
-		SELECTED_THREADS.add("4bqcc3");
-		SELECTED_THREADS.add("5oyz3b");
-		SELECTED_THREADS.add("21f3as");
-		SELECTED_THREADS.add("2t65ql");
-		SELECTED_THREADS.add("4d75i7");
-		SELECTED_THREADS.add("5exz2e");
-		SELECTED_THREADS.add("67ivae");
-		SELECTED_THREADS.add("6cqdye");
-		SELECTED_THREADS.add("5uzetf");
-		SELECTED_THREADS.add("5qqa51");
-		SELECTED_THREADS.add("5whpqs");
-		SELECTED_THREADS.add("1wl9ic");
-		SELECTED_THREADS.add("2s7obx");
-		SELECTED_THREADS.add("5n58sm");
+		SELECTED_THREADS.add("worldnews/36ny58");
+		SELECTED_THREADS.add("worldnews/4bqcc3");
+		SELECTED_THREADS.add("worldnews/5oyz3b");
+		SELECTED_THREADS.add("worldnews/21f3as");
+		SELECTED_THREADS.add("worldnews/2t65ql");
+		SELECTED_THREADS.add("worldnews/4d75i7");
+		SELECTED_THREADS.add("worldnews/5exz2e");
+		SELECTED_THREADS.add("worldnews/67ivae");
+		SELECTED_THREADS.add("worldnews/6cqdye");
+		SELECTED_THREADS.add("worldnews/5uzetf");
+		SELECTED_THREADS.add("worldnews/5qqa51");
+		SELECTED_THREADS.add("iama/5whpqs");
+		SELECTED_THREADS.add("iama/1wl9ic");
+		SELECTED_THREADS.add("iama/2s7obx");
+		SELECTED_THREADS.add("iama/5n58sm");
 	}
 	
 
